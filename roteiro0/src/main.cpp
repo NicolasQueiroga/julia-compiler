ad3406c80d5c06510fcca8805c06597ddc890163
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2023-02-07 14:12:47 -0300

    roterio0 `complete`

---
+++ roteiro0/src/main.cpp
@@ -0,0 +1,70 @@
+// make a simple compiler that receives a expression string only with + and - and returns the result. it can have spaces between the operators and operands.
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using std::cout;
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        cout << "Usage: " << argv[0] << " <expression>\n";
+        return 1;
+    }
+
+    
+    std::string token;
+    std::vector<std::string> tokens;
+    std::string expression = argv[1];
+    for (char c : expression)
+    {
+        if (c == '+' || c == '-' || c == ' ')
+        {
+            if (!token.empty())
+            {
+                tokens.push_back(token);
+                token.clear();
+            }
+            if (c != ' ')
+                tokens.push_back(std::string(1, c));
+        }
+        else
+            token += c;
+    }
+    tokens.push_back(token);
+    tokens[tokens.size() - 1] == "" ? tokens.pop_back() : void();
+
+    for (std::string s : tokens)
+        cout << s << "\n";
+
+    int result = 0;
+    bool isNegative = false;
+    for (int i = 0; i < tokens.size(); i++)
+    {
+        if (i % 2 == 0 && (tokens[i] == "+" || tokens[i] == "-"))
+        {
+            cout << "Invalid expression\n";
+            return 1;
+        }
+        if (i % 2 == 1 && (tokens[i] != "+" && tokens[i] != "-"))
+        {
+            cout << "Invalid expression\n";
+            return 1;
+        }
+        if (i % 2 != 0)
+            isNegative = tokens[i] == "-" ? true : false;
+        else
+        {
+            if (isNegative)
+                result -= std::stoi(tokens[i]);
+            else
+                result += std::stoi(tokens[i]);
+        }
+    }
+
+    cout << result << "\n";
+    return 0;
+}
