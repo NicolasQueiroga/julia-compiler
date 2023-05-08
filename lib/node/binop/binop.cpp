7da714e8eb243de67b3978e376624d38c807e8b4
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2023-05-08 17:17:53 -0300

    agr vai

--- lib/node/binop/binop.cpp
+++ lib/node/binop/binop.cpp
@@ -1,47 +1,10 @@
 #include "binop.hpp"
-#include <type_traits>
+#include "utils.hpp"
+#include <iostream>
 
 BinOp::BinOp(std::vector<Node *> children, std::variant<int, std::string> value)
     : children(children), value(value)
 {
-}
-
-int BinOp::operateInt(std::string op, int left, int right)
-{
-    if (op == "+")
-        return left + right;
-    else if (op == "-")
-        return left - right;
-    else if (op == "*")
-        return left * right;
-    else if (op == "/")
-        return left / right;
-    else if (op == "||")
-        return left || right;
-    else if (op == "&&")
-        return left && right;
-    else if (op == "<")
-        return left < right;
-    else if (op == ">")
-        return left > right;
-    else if (op == "<=")
-        return left <= right;
-    else if (op == ">=")
-        return left >= right;
-    else if (op == "==")
-        return left == right;
-    else if (op == "!=")
-        return left != right;
-    else
-        throw "Expected PLUS or MINUS or MUL or DIV or OR or AND";
-}
-
-std::string BinOp::operateString(std::string op, std::string left, std::string right)
-{
-    if (op == "+" || op == ".")
-        return left + right;
-    else
-        throw "Expected PLUS or OR or AND";
 }
 
 ValueType BinOp::Evaluate()
@@ -49,33 +12,55 @@ ValueType BinOp::Evaluate()
     if (std::holds_alternative<std::string>(value))
     {
         std::string op = std::get<std::string>(value);
+        ValueType left = children[0]->Evaluate();
+        ValueType right = children[1]->Evaluate();
 
-        if (std::holds_alternative<std::string>(children[0]->Evaluate()) && std::holds_alternative<std::string>(children[1]->Evaluate()))
+        if (op == ".")
         {
-            std::string left = std::get<std::string>(children[0]->Evaluate());
-            std::string right = std::get<std::string>(children[1]->Evaluate());
-            return operateString(op, left, right);
+            if (std::holds_alternative<int>(left))
+                left = std::to_string(std::get<int>(left));
+            if (std::holds_alternative<int>(right))
+                right = std::to_string(std::get<int>(right));
+            return std::get<std::string>(left) + std::get<std::string>(right);
         }
-        else if (std::holds_alternative<int>(children[0]->Evaluate()) && std::holds_alternative<std::string>(children[1]->Evaluate()))
+        else if (op == "+")
         {
-            std::string left = std::to_string(std::get<int>(children[0]->Evaluate()));
-            std::string right = std::get<std::string>(children[1]->Evaluate());
-            return operateString(op, left, right);
-        }
-        else if (std::holds_alternative<std::string>(children[0]->Evaluate()) && std::holds_alternative<int>(children[1]->Evaluate()))
-        {
-            std::string left = std::get<std::string>(children[0]->Evaluate());
-            std::string right = std::to_string(std::get<int>(children[1]->Evaluate()));
-            return operateString(op, left, right);
+            if (std::holds_alternative<std::string>(left) && std::holds_alternative<std::string>(right))
+                return std::get<std::string>(left) + std::get<std::string>(right);
+            else if (std::holds_alternative<int>(left) && std::holds_alternative<std::string>(right))
+                return std::to_string(std::get<int>(left)) + std::get<std::string>(right);
+            else if (std::holds_alternative<std::string>(left) && std::holds_alternative<int>(right))
+                return std::get<std::string>(left) + std::to_string(std::get<int>(right));
+            else
+                throw "Invalid operands for +";
         }
         else
         {
-            int left = std::get<int>(children[0]->Evaluate());
-            int right = std::get<int>(children[1]->Evaluate());
-            return operateInt(op, left, right);
+            if (op == "-" && std::holds_alternative<int>(left) && std::holds_alternative<int>(right))
+                return std::get<int>(left) - std::get<int>(right);
+            else if (op == "*" && std::holds_alternative<int>(left) && std::holds_alternative<int>(right))
+                return std::get<int>(left) - std::get<int>(right);
+            else if (op == "/" && std::holds_alternative<int>(left) && std::holds_alternative<int>(right))
+                return std::get<int>(left) - std::get<int>(right);
+            else if (op == "||" && std::holds_alternative<int>(left) && std::holds_alternative<int>(right))
+                return std::get<int>(left) - std::get<int>(right);
+            else if (op == "&&" && std::holds_alternative<int>(left) && std::holds_alternative<int>(right))
+                return std::get<int>(left) - std::get<int>(right);
+            else if (op == "<")
+                return left < right;
+            else if (op == ">")
+                return left > right;
+            else if (op == "<=")
+                return left <= right;
+            else if (op == ">=")
+                return left >= right;
+            else if (op == "==")
+                return left == right;
+            else if (op == "!=")
+                return left != right;
+            else
+                throw "Expected PLUS or MINUS or MUL or DIV or OR or AND";
         }
-        
-        return 0;
     }
     else
         throw "Expected string";
