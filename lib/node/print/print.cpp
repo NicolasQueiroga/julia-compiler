442b4c9945ae57e1184dd4aced9fb93edd0753b9
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2023-05-14 17:52:05 -0300

    removendo print

--- lib/node/print/print.cpp
+++ lib/node/print/print.cpp
@@ -9,7 +9,8 @@ Print::Print(std::vector<Node *> children) : children(children)
 
 ValueType Print::Evaluate()
 {
-    std::cout << this->children[0]->Evaluate() << std::endl;
+    // std::cout << this->children[0]->Evaluate() << std::endl;
+    this->children[0]->Evaluate();
     Assembler::incrementAsmCode("PUSH EBX\nCALL print\nPOP EBX");
     return 0;
 }
